4ea398d638bd3eebf33fa805fad8106f46625c78
Pascal Fischer <pascal.fischer@fondof.de>
2018-10-25 13:51:11 +0200

    testing get url with correct protocol

--- src/FondOfSpryker/Yves/OpenGraph/Business/Model/Property.php
+++ src/FondOfSpryker/Yves/OpenGraph/Business/Model/Property.php
@@ -15,10 +15,15 @@ class Property implements PropertyInterface
     const TYPE_WEBSITE = "website";
     const TYPE_PRODUCT = "product";
 
+    const SERVER_HTTPS = 'HTTPS';
+    const SERVER_PORT = 'SERVER_PORT';
+    const SERVER_HTTP_HOST = 'HTTP_HOST';
+    const SERVER_REQUEST_URI = 'REQUEST_URI';
+
     /**
      * @param array $params
      *
-     * @return Generated\Shared\Transfer\OpenGraphPropertiesTransfer OpenGraphPropertiesTransfer
+     * @return \Generated\Shared\Transfer\OpenGraphPropertiesTransfer OpenGraphPropertiesTransfer
      */
     public function getProperties(array $params): OpenGraphPropertiesTransfer
     {
@@ -26,12 +31,27 @@ class Property implements PropertyInterface
 
         $transferObject->setTitle($params['title']);
         $transferObject->setType($params['type']);
-        $transferObject->setUrl($params['url']);
+        $transferObject->setUrl($this->getHost());
         $transferObject->setDescription($params['description']);
         $transferObject->setImage($params['image']);
         $transferObject->setSiteName($params['site_name']);
         $transferObject->setAvailability($params['availability']);
 
         return $transferObject;
+    }
+
+    /**
+     * Using global server variables instead HttpFoundation Request
+     * because there is an issue while detecting https protocoll
+     *
+     * @return string
+     */
+    protected function getHost(): string
+    {
+        $protocol = (!empty($_SERVER[static::SERVER_HTTPS]) && $_SERVER[static::SERVER_HTTPS] !== 'off' || $_SERVER[static::SERVER_PORT] == 443) ? "https://" : "http://";
+        $domainName = $_SERVER[static::SERVER_HTTP_HOST];
+        $uri = ($_SERVER[static::SERVER_REQUEST_URI]) ?: '';
+
+        return $protocol . $domainName . $uri;
     }
 }
